e6cdbf0e739010b8a42f85ca51d9eba912086cde
Soleev Maksim <msoleev98@gmail.com>
2023-03-10 01:42:19 +0300

    authorization and transition done

--- uchpr/ViewModels/AuthorizationWindowViewModel.cs
+++ uchpr/ViewModels/AuthorizationWindowViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Operators;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using uchpr.Utilities;
+using uchpr.Utilities.Commands;
 using uchpr.ViewModels.Base;
+using uchpr.Views.AuthorizationWindow;
 
 namespace uchpr.ViewModels
 {
@@ -21,6 +28,18 @@ namespace uchpr.ViewModels
             set
             {
                 Set(ref _connectionStatus, value);
+            }
+        }
+        #endregion
+
+        #region Статус Входа
+        private string _authorizationStatus = "...";
+        public string AuthorizationStatus
+        {
+            get => _authorizationStatus;
+            set
+            {
+                Set(ref _authorizationStatus, value);
             }
         }
         #endregion
@@ -49,26 +68,9 @@ namespace uchpr.ViewModels
         }
         #endregion
 
+
+        MainWindow mainWindow;
         public AuthorizationWindowViewModel()
-        {
-            UserAuthentification();
-        }
-
-        //public string SqlCommand(string query, MySqlConnection connection)
-        //{
-        //    MySqlCommand command = new MySqlCommand(query, connection);
-
-        //    MySqlDataReader reader = command.ExecuteReader();
-        //    while (reader.Read())
-        //    {
-        //        reader[0].ToString();
-        //    }
-        //    reader.Close();
-
-        //    return reader[0].ToString();
-        //}
-
-        public void UserAuthentification()
         {
             MySqlConnection connection = DBUtilities.GetDBConnection();
             try
@@ -77,10 +79,53 @@ namespace uchpr.ViewModels
                 connection.Open();
                 _connectionStatus = "Connection successeful!";
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 _connectionStatus = "Connection error!";
+                connection.Close();
             }
+            UserAuthentificationCommand = new ActionCommand(OnUserAuthentificationCommandExecuted, CanUserAuthentificationCommandExecute);
+        }
+
+        #region Commands
+
+        #region UserAuthentificationCommand
+        public ICommand UserAuthentificationCommand { get; }
+
+        private bool CanUserAuthentificationCommandExecute(object obj) => true;
+
+        private void OnUserAuthentificationCommandExecuted(object obj)
+        {
+            UserAuthentification();
+        }
+        #endregion
+
+        #endregion
+
+        public void UserAuthentification()
+        {
+            string queryString = "select * from employee where login = @login and password = @password";
+            DataTable dataTable = new();
+            MySqlDataAdapter dataAdapter = new();
+
+            MySqlCommand sqlCommand = new(queryString, DBUtilities.GetDBConnection());
+            sqlCommand.Parameters.Add("@login", MySqlDbType.VarChar).Value = _login;
+            sqlCommand.Parameters.Add("@password", MySqlDbType.VarChar).Value = _password;
+
+            dataAdapter.SelectCommand = sqlCommand;
+            dataAdapter.Fill(dataTable);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                AuthorizationStatus = "Авторизован";
+                mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                AuthorizationStatus = "Не удалось авторизировать";
+            }
+
         }
     }
 }
---
+++ uchpr/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uchpr.Utilities.Commands;
+using uchpr.Utilities;
+using uchpr.ViewModels.Base;
+
+namespace uchpr.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+            MySqlConnection connection = DBUtilities.GetDBConnection();
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
